8a611d03f9bbf310082db9bdee1e50569fe3d903
TuZz04 <augustin.caillet@gmail.com>
2023-09-29 14:45:57 +0200

    Dz

--- python/myapp/Data/authors.py
+++
@@ -1,7 +0,0 @@
-authors = [
-    { "id" : 0, 
-        "last_name": "Tolkien", "first_name": "J.R.R.", "nationality": "British", "famous_work": "The Lord of the Rings"},
-    { "id" : 1, "last_name": "Rowling", "first_name": "J.K.", "nationality": "British", "famous_work": "Harry Potter"},
-    {"id" : 2, "last_name": "Hemingway", "first_name": "Ernest", "nationality": "American", "famous_work": "The Old Man and the Sea"},
-    {"id" : 3, "last_name": "Austen", "first_name": "Jane", "nationality": "British", "famous_work": "Pride and Prejudice"}
-]
